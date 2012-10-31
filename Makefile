158756048cd74c487b09fb0ec53aa1df0d1d82bc
Jason Grout <jason.grout@drake.edu>
2012-10-30 23:29:19 -0500

    Consolidate the two approaches into one source file so that everything except the actual popcount step is exactly the same.

--- Makefile
+++ Makefile
@@ -4,11 +4,11 @@ COPT = -O3 -msse4.2
 all: popcnt lookup
 
 popcnt: popcnt.c timer.h
-	$(CC) $(COPT) popcnt.c -o popcnt
+	$(CC) $(COPT) -DPOPCOUNT popcnt.c -o popcnt
 
-lookup: lookup.c timer.h
-	$(CC) $(COPT) lookup.c -o lookup
+lookup: popcnt.c timer.h
+	$(CC) $(COPT) -DLOOKUP popcnt.c -o lookup
 
-asm: popcnt.c lookup.c timer.h
-	$(CC) -S $(COPT) popcnt.c -o popcnt.asm
-	$(CC) -S $(COPT) lookup.c -o lookup.asm
+asm: popcnt.c timer.h
+	$(CC) -S $(COPT) -DPOPCOUNT popcnt.c -o popcnt.asm
+	$(CC) -S $(COPT) -DLOOKUP  popcnt.c -o lookup.asm
--- popcnt.c
+++ popcnt.c
@@ -6,25 +6,45 @@
 
 #define MAX_NUM 10000
 
+#ifdef POPCOUNT
+#define POP popcnt[i] = __builtin_popcount(values[i])
+#endif
+
+#ifdef LOOKUP
+#define POP popcnt[i] = popcnt_lookup[values[i] >> 8] + popcnt_lookup[values[i] & 0x00FF]
+#endif
+
 int main(int argc, char **argv) {
   uint8_t popcnt_lookup[256];
   uint16_t values[MAX_NUM];
   uint8_t popcnt[MAX_NUM];
   uint32_t i, j;
   uint64_t t;
+  uint64_t sum = 0;
+  uint64_t time = 0;
 
   srandom(0);
   for(i=0; i<MAX_NUM; i++) {
     values[i] = random() & 0xFFFF;
   }
+  for(i=0; i<256; i++) {
+    popcnt_lookup[i] = __builtin_popcount(i);
+  }
 
-  timer(&t); // initialize timer
   for(j=0; j<100; j++) {
+
+    timer(&t); // initialize timer
     for(i=0; i<MAX_NUM; i++) {
-      popcnt[i] = __builtin_popcount(values[i]);
+      POP;
+    }
+    time += timer(&t);
+
+    for(i=0; i<MAX_NUM; i++) {
+      // we need to use popcnt so that the compiler doesn't optimize it away
+      sum += popcnt[i];
     }
   }
-  printf("Time: %0.3f ms\n", timer(&t)/1000.0);
+  printf("Time: %0.3f ms\n", time/1000.0);
+  printf("Hash: %llu\n", sum);
   return 0;
 }
-
